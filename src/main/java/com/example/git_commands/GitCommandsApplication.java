5c45a00b43f4eb91ff9a58907434774c1c3b7d4e
subashpernandas <subashpstalin@gmail.com>
2026-06-14 07:49:59 +0530

    create a spring -boot project for git commands

---
+++ src/main/java/com/example/git_commands/GitCommandsApplication.java
@@ -0,0 +1,13 @@
+package com.example.git_commands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCommandsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitCommandsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/git_commands/GitCommandsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_commands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitCommandsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
