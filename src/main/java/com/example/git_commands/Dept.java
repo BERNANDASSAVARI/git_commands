e822b7e8411bc13da8ce863832a34e47ff0bc63b
BERNANDASSAVARI <subashpstalin@gmail.com>
2026-06-14 09:28:37 +0530

    undo the RestController

--- src/main/java/com/example/git_commands/Dept.java
+++ src/main/java/com/example/git_commands/Dept.java
@@ -1,7 +1,9 @@
 package com.example.git_commands;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class Dept {
 }
